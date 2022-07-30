18bbb48baf8c35ecd224ff871900c0da94f4557d
itm94lj <itm94lj@163.com>
2022-07-30 16:51:33 +0800

    use mysql as basic authentication storage

---
+++ template/src/main/java/com/itm94lj/template/signin/DataBaseConfig.java
@@ -0,0 +1,20 @@
+package com.itm94lj.template.signin;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataBaseConfig {
+
+    @Bean
+    @ConfigurationProperties("spring.datasource")
+    public DataSource dataSource() {
+
+        return DataSourceBuilder.create()
+                .build();
+    }
+}
